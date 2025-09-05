bc182ee931e18c969d5bfb620b9faba6238f4e2d
Jill Kok, San Mou <103614420+hhsw2015@users.noreply.github.com>
2025-09-05 19:44:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,15 @@
 
 ./cf logs app_cf --recent
 
+
+# or https://www.youtube.com/watch?v=zuLaPkHmmQA
+
+#cf TUNNEL: http://127.0.0.1:10086
+
+./cf push app_cf –docker-image ghcr.io/uncleluogithub/sapcf:latest -m 512M -k 512M –health-check-type port –no-route –no-start
+./cf set-env app_cf APP_UUID 7bd57098-82bd-4dfa-b32c-9943a52d354f
+./cf set-env app_cf VMESS_HOST YOUR_CLOUDFLARE_TUNNEL_DOMAIN
+./cf set-env app_cf TUNNEL_TOKEN ‘YOUR_TUNNEL_TOKEN_HERE’
+./cf set-health-check app_cf port
+./cf start app_cf
+./cf logs app_cf –recent
